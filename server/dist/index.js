0f44bb5c38755bfc8feeec5617f6946d8dfca235
VinceNguyen <nguyenquangv95@gmail.com>
2022-04-20 20:22:37 +0700

    connect-to-DB

--- server/dist/index.js
+++
@@ -1,2 +0,0 @@
-console.log("HI");
-//# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log("HI");
+require("dotenv").config();
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+
+const main = async () => {
+  const AppDataSource = new DataSource({
+    type: "postgres",
+    database: "jwt-authentication",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
+  await AppDataSource.initialize().catch((err) =>
+    console.log("Error starting server", err)
+  );
+};
+
+main();
