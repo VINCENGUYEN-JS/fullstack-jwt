5aa04b902efdd5379d5a593bea86db253765e70b
VinceNguyen <nguyenquangv95@gmail.com>
2022-04-28 19:59:19 +0700

    set-up-fe

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>JWT AUTHENTICATION FULLSTACK</h1>
+      <nav
+        style={{
+          borderBottom: "1px solid",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="">Home</Link> |<Link to="login">Login</Link> |{" "}
+        <Link to="register">Register</Link> | <Link to="profile">Profile</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
