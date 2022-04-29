3148374a450620c00b8bd0ee508f3b6f5ecbd5b5
VinceNguyen <nguyenquangv95@gmail.com>
2022-04-29 17:10:42 +0700

    add-forms-for-clients

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [userName, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <form style={{ marginTop: "1rem" }}>
+      <input
+        type="text"
+        value={userName}
+        placeholder="UserName"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [userName, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <form style={{ marginTop: "1rem" }}>
+      <input
+        type="text"
+        value={userName}
+        placeholder="UserName"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
 };
 
 export default Register;
